83c8a0b832e74578e51bb347e083690a2da688fd
kapilyadav <singhkapil347@gmail.com>
2024-09-14 15:29:27 +0530

    Added AWS Demo App

---
+++ SpringAwsDemoApp/src/main/java/com/kapil/springawsdemoapp/SpringAwsDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.kapil.springawsdemoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsDemoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsDemoAppApplication.class, args);
+    }
+
+}
---
+++ SpringAwsDemoApp/src/test/java/com/kapil/springawsdemoapp/SpringAwsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kapil.springawsdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsDemoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
