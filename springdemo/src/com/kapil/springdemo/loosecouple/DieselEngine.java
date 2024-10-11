e039629db506edf75c592716073a2761ff045f8d
kapilyadav <singhkapil347@gmail.com>
2024-10-11 14:25:16 +0530

    Updated Indices

--- springdemo/src/com/kapil/springdemo/loosecouple/DieselEngine.java
+++
@@ -1,8 +0,0 @@
-package com.kapil.springdemo.loosecouple;
-
-public class DieselEngine implements IEngine{
-    @Override
-    public void start() {
-        System.out.println("Petrol Engine Started");
-    }
-}
--- springdemo/src/com/kapil/springdemo/loosecouple/IEngine.java
+++
@@ -1,5 +0,0 @@
-package com.kapil.springdemo.loosecouple;
-
-public interface IEngine {
-    void start();
-}
